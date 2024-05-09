708974d907d674163306907353e3e11aca192b2f
Arthur Cardoso <arthurmcardoso99@gmail.com>
2024-05-08 21:37:05 -0300

    class biding

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'Aula12' title`, () => {
+  it(`should have the 'introducao-angular' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Aula12');
+    expect(app.title).toEqual('introducao-angular');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, Aula12');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, introducao-angular');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,23 @@
+
+// @Component({
+//   selector: 'app-root',
+//   standalone: true,
+//   imports: [RouterOutlet],
+//   templateUrl: './app.component.html',
+//   styleUrl: './app.component.css'
+// })
+// export class AppComponent {
+//   title = 'Aula12';
+
+
+//   desabilitado: boolean = true;
+
+//   imageUrl: String = "https://fastly.picsum.photos/id/1/200/200.jpg?hmac=jZB9EZ0Vtzq-BZSmo7JKBBKJLW46nntxq79VMkCiBG8"
+
+
+// }
+
+
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
@@ -9,12 +29,7 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Aula12';
+  title = 'introducao-angular';
 
-
-  // desabilitado: boolean = true;
-
-  imageUrl: String = "https://fastly.picsum.photos/id/1/200/200.jpg?hmac=jZB9EZ0Vtzq-BZSmo7JKBBKJLW46nntxq79VMkCiBG8"
-
-
+  urlImagem = 'http://lorempixel.com.br/400/200';
 }
